b301ec33cbc0de1c3826f5cf706f628bc368d77e
Pete Keen <peter.keen@bugsplat.info>
2012-12-11 21:04:06 -0800

    Add --without-doc-strings option

--- formula/python-2.7.3.rb
+++ formula/python-2.7.3.rb
@@ -2,4 +2,8 @@ class Python273 < HerokuBrew::Formula
   url     "http://www.python.org/ftp/python/2.7.3/Python-2.7.3.tar.bz2"
   basedir "Python-2.7.3"
   depends "sqlite3"
+
+  def install
+    autoconf(['--without-doc-strings'])
+  end
 end
