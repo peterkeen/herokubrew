165c8e9a21ce50fa8e3c11690d41da42a1bf5125
Andrew Lorente <andrew.lorente@gmail.com>
2013-04-16 20:52:25 -0700

    Spackle over a shortcoming in libjpeg's makefile
    It seems to fail to support --prefix properly

--- formula/libjpeg-6b.rb
+++ formula/libjpeg-6b.rb
@@ -6,4 +6,13 @@ class Libjpeg6b < HerokuBrew::Formula
   # I've chosen a mirror that should be relatively close to US-East.
   url 'http://iweb.dl.sourceforge.net/project/libjpeg/libjpeg/6b/jpegsrc.v6b.tar.gz'
   basedir 'jpeg-6b'
+
+  def autoconf(options=[])
+    options = options.join(" ")
+    run("./configure --prefix=#{prefix} #{options}")
+    run("make")
+    run("mkdir -p #{prefix}/bin")
+    run("mkdir -p #{prefix}/man/man1")
+    run("make install")
+  end
 end
